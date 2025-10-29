f03910b63a52603df4bb4c664d0920fdcb394fc8
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2025-10-29 11:44:03 +0530

    update

--- 7.) OOP's.py
+++ 7.) OOP's.py
@@ -1,5 +1,15 @@
 # class and object
 
+# Python is an object oriented programming language.
+
+# Almost everything in Python is an object, with its properties and methods.
+
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+
+
+
+
+
 class student:
     #attributes
     roll_no = 0
@@ -43,3 +53,28 @@ s2.name = "aryan"
 
 print(s1.roll_no)
 print(s2.name)
+
+
+
+
+class student:
+    #attributes
+    roll_no =0
+    name=""
+    age=""
+    gender=""
+    #method 1
+    #self refers to the current instance of the class
+    def display_info(self):
+        print(self.name, self.roll_no, self.age, self.gender)
+    #method 2
+    def set_info(self):
+        self.name = input("Enter the student name")
+        self.roll_no = input("Enter the Roll Number")
+        self.age = input("Enter the Age")
+        self.gender = input("Enter the gender")
+
+s1 = student()
+
+s1.set_info()
+s1.display_info()
---
+++ 8.) init__Method.py
@@ -0,0 +1,11 @@
+class classplus:
+    # attribute
+    brand_color = "blue"
+
+    # method_constructor
+    def __init__(self):
+        print("constructor is called")
+
+
+pst = classplus()
+
