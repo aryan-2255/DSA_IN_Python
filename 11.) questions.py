5e3f373c14b72dbb3776de5570785181ef5faafd
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2025-10-30 15:11:23 +0530

    update

--- 11.) questions.py
+++ 11.) questions.py
@@ -18,3 +18,34 @@ class BankAccount:
         
 b1 = BankAccount()
 b1.withdraw()
+
+
+# question 
+
+class BankAccount:
+     
+
+     def __init__(self, a_n, ba):
+          self.account_number = a_n
+          self.balance = ba
+    
+     def withraw(self,amt):
+        if(amt > self.balance):
+             raise RuntimeError("insufficent balance")
+        if(amt < 0):
+             raise ValueError("invalid input")
+     
+
+p1 = BankAccount(1234,1000.0)
+try:
+     try:
+        try:
+            p1.withraw(1500)
+        except RuntimeError as msg:
+          print(msg)
+     except ValueError as msg:
+        print(msg)
+except Exception:
+     print("unknown error")
+    
+
